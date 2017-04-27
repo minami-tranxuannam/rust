a10684ff12826bc081b777d3dec10655d5efba29
Tran Xuan Nam <tran.xuan.nam@framgia.com>
2017-04-27 18:53:38 +0700

    chapter_fourth

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,34 @@
-extern crate rand;
-
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
-
 fn main() {
-    println!("Guess the number");
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let x = 5;
+    let (x, y) = (1, 2);
+    let x: i32 = 5; // signed integer 32 bites
 
-    loop {
-        println!("Please input your guess.");
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Failed to read line");
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        
-        println!("Your guess: {}", guess);
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
+    // by default bidings are immutable
+    // let y = 5;
+    // y = 10;
+    // ^^^^^^ re-assignment of immutable variable
+
+    let mut z = 5;
+    z = 10;
+    // OK
+
+    // let a: i32;
+    // println!("The value of a is {}", a);
+
+    // scope
+
+    let b: i32 = 17;
+    {
+        let c: i32 = 5;
+        println!("The value of x is {} and value of y is {}", b, c); // OK
     }
+    // println!("The value of x is {} and value of y is {}", b, c); // not OK
+
+
+    let mut x: i32 = 13;
+    x = 7;
+    let x = x;
+
+    let y = 4;
+    let y = "I can also be bound to text!";
 }
